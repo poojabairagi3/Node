4eb305f776665017c327845910fd0d9a077d4eb8
poojabairagi3 <poojabairagi0301@gmail.com>
2023-04-12 11:41:08 +0530

    for parse the body commit

--- app.js
+++ app.js
@@ -15,17 +15,25 @@
 
 
 const express =require('express');
+const bodyparser=require('body-parser');
 
 const app =express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
-    next(); //allows the request to continue to the middleware to line
+app.use(bodyparser.urlencoded({extended:true})); //for parsing form 
+
+
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="number" name="size"><button type="submit">Add Product</button> </form>');
+    
 });
 
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
 
-app.use((req,res,next)=>{
-    console.log('In another middleware');
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello from Express!</h1>');
 });
 
