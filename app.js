8e2bfef0badad13974023bbf7fd25d58df8755f6
poojabairagi3 <poojabairagi0301@gmail.com>
2023-04-19 20:53:02 +0530

    group chat app

--- app.js
+++ app.js
@@ -1,48 +1,30 @@
-// node .js
 
 
-// // import http from 'http';
-// const http =require('http');
-// const routes =require('./h8routes');
-// console.log(routes.someText);
-// const server = http.createServer(routes.handler);
-// server.listen(3000);
 
 
-// //Express.js
-// const express =require('express');
-// const bodyparser=require('body-parser');
-// const app =express();
-// app.use(bodyparser.urlencoded({extended:true})); //for parsing form 
-// app.use('/add-product',(req,res,next)=>{
-//     res.send('<form action="/product" method="POST"><input type="text" name="product"><input type="number" name="size"><button type="submit">Add Product</button> </form>');   
-// });
-// app.post('/product',(req,res,next)=>{
-//     console.log(req.body);
-//     res.redirect('/');
-// });
+const express=require('express');
+
+const app=express();
+
+const routeAdmin=require('./routes/admin');
+
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+// app.use('/login',(req,res,next)=>{
+   
+//     res.send('<form action="/"  method="post"><input type="text" name="name" placeholder="Enter your name"><button type="submit">log in</button></form>')
+    
+// })
 // app.use('/',(req,res,next)=>{
-//     res.send('<h1>Hello from Express!</h1>');
-// });
-// app.listen(3000);
+//     console.log(req.body)
+//     res.send('<form action="/"  method="post"><input type="text" name="message" placeholder="type a message"><button type="submit">send</button></form>')
+// })
+app.use(routeAdmin);
 
-
-// Express routing 
-const express =require('express');
-const bodyparser=require('body-parser');
-
-const app =express();
-
-const adminRoutes=require('./routes/admin.js');
-const shopRoutes=require('./routes/shop.js');
-
-app.use(bodyparser.urlencoded({extended:true})); //for parsing form 
-
-app.use('/admin',adminRoutes);
-app.use('/shop',shopRoutes);
-
-app.use((req,res)=>{
-    res.status(404).send('<h1>Page not found</h1>')
+app.post('/',(req,res,next)=>{
+    res.status(404).send('<h1>Error 404 This Page is Not Founded</h1>')
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,43 @@
-const express =require('express');
 
-const  router =express.Router();
 
-//  /admin/add-product =>GET
-router.get('/add-product',(req,res,next)=>{
-    
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="product"><input type="number" name="size"><button type="submit">Add Product</button> </form>');
-    
+const express = require('express');
+
+const fs = require('fs');
+
+const router = express.Router();
+
+router.get('/login', (req, res, next) => {
+
+    res.send('<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/" method="GET"><input id="username" placeholder="Enter your name" type="text" name="title"><button type="submit">add</button></form>');
+
+})
+
+
+router.get('/', (req, res, next) => {
+    fs.readFile('chat.txt', (err, data) => {
+        if (err) {
+            console.group(err);
+            data = 'No chat Exits'
+        }
+        res.send(`${data}
+    <form action="/sec" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method="POST">
+    <input type="text" id="message" name="message" placeholder="type a message">
+    <input type="hidden" name="username" id="username">
+    <button typr="submit">send</button>
+    </form>
+    `)
+    })
+
 });
 
-//  /admin/add-product  =>POST
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/shop');
+router.post('/sec', (req, res, next) => {
+    console.log(req.body.username);
+    console.log(req.body.message);
+    fs.writeFile("chat.txt", `${req.body.username}:${req.body.message}  `, { flag: 'a' }, (err) => {
+        err ? console.log(err) : res.redirect('/');
+    });
+
 });
 
-module.exports=router;
+
+module.exports = router;
